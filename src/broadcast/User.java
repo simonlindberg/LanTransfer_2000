3dd7126795d182101dbfff36b3e6024d3d114569
Firas <firas.dib@gmail.com>
2014-03-26 13:20:33 +0100

    woops

---
+++ src/broadcast/User.java
@@ -0,0 +1,55 @@
+package broadcast;
+
+public class User {
+
+	private String ip;
+	private int port;
+	private String username;
+
+	public User(String username, String ip, int port) {
+		this.ip = ip;
+		this.port = port;
+		this.username = username;
+	}
+
+	public String toString() {
+		return username + ": " + ip + ":" + port;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ip == null) ? 0 : ip.hashCode());
+		result = prime * result
+				+ ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (ip == null) {
+			if (other.ip != null)
+				return false;
+		} else if (!ip.equals(other.ip))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	public String getUsername() { return username; }
+	public String getIP() { return ip; }
+	public int getPort() { return port; }
+	
+}
