7c444c6e6ffe1eb495323d1685061ded81a4dade
Firas <firas.dib@gmail.com>
2014-03-27 20:30:45 +0100

    MIGLAYOUT I LOVE YOU

--- src/GUI/ChatFrame.java
+++ src/GUI/ChatFrame.java
@@ -5,8 +5,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -22,22 +21,32 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import org.w3c.dom.css.CSSPrimitiveValue;
+import org.w3c.dom.css.RGBColor;
+
+import net.miginfocom.swing.MigLayout;
+
 public class ChatFrame extends JFrame {
 
 	private static final long serialVersionUID = -2013659249126443168L;
 
 	private User user;
-	private int chatNum;
+	private static final Color MY_BG = new Color(244, 244, 244);
+	private static final Color INFO_TXT = new Color(195, 195, 195);
+	private static final Color TXT_COLOR = new Color(43, 43, 43);
+	private User lastMessage;
 
 	public ChatFrame(final User user) {
 		this.user = user;
-		chatNum = 0;
+		lastMessage = null;
+
 		setLayout(new BorderLayout());
 
 		createComponents();
 
 		setTitle("Chat window with " + user.getUsername());
 		setSize(new Dimension(500, 600));
+		// setMinimumSize(new Dimension(300, 400));
 		setResizable(false);
 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -49,17 +58,21 @@ public class ChatFrame extends JFrame {
 	}
 
 	private void createComponents() {
-		final JPanel chatLog = new JPanel(new GridBagLayout());
+		// final JPanel chatLog = new JPanel(new GridBagLayout());
+		MigLayout layout = new MigLayout("gap rel 0, wrap 1, insets 0");
+		final JPanel chatLog = new JPanel(layout);
 		// chatLog.setLayout(new BoxLayout(chatLog, BoxLayout.PAGE_AXIS));
 
 		final JScrollPane scrollChatLog = new JScrollPane(chatLog);
-		chatLog.setAutoscrolls(true);
-		scrollChatLog.setAutoscrolls(true);
+		scrollChatLog.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		// chatLog.setAutoscrolls(true);
+		// scrollChatLog.setAutoscrolls(true);
 
-		chatLog.setBackground(Color.red);
+		chatLog.setBackground(Color.WHITE);
 
-//		String startMessage = "Started chat with " + user.getUsername() + " at " + (new Date()).toString();
-//		chatLog.add(new JLabel(startMessage));
+		// String startMessage = "Started chat with " + user.getUsername() +
+		// " at " + (new Date()).toString();
+		// chatLog.add(new JLabel(startMessage));
 		add(scrollChatLog, BorderLayout.CENTER);
 
 		final JPanel chatInput = new JPanel(new BorderLayout());
@@ -73,7 +86,7 @@ public class ChatFrame extends JFrame {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-					send(input.getText(), chatLog);
+					send(input.getText(), chatLog, scrollChatLog);
 					input.setText("");
 				}
 			}
@@ -83,59 +96,62 @@ public class ChatFrame extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				send(input.getText(), chatLog);
+				send(input.getText(), chatLog, scrollChatLog);
 				input.setText("");
 			}
 		});
+
+		// scrollChatLog.getVerticalScrollBar().addAdjustmentListener(new
+		// AdjustmentListener() {
+		// public void adjustmentValueChanged(AdjustmentEvent e) {
+		// e.getAdjustable().setValue(e.getAdjustable().getMaximum());
+		// }
+		// });
 	}
 
-	private void send(final String text, final JComponent chatLog) {
+	private void send(final String text, final JComponent chatLog, final JComponent scrollChatLog) {
 		if (!text.equals("")) {
 			System.out.println(text);
-			final JPanel messageContents = new JPanel(new BorderLayout());
+			final JPanel messageContents = new JPanel();
+			messageContents.setLayout(new MigLayout("insets 0, gap rel 0", "10[]10[]10[]10", "5[]5"));
+			messageContents.setBackground(MY_BG);
+
 			final JTextArea contents = new JTextArea(text);
 			contents.setEditable(false);
 			contents.setLineWrap(true);
 			contents.setWrapStyleWord(true);
 			contents.setAlignmentX(Component.LEFT_ALIGNMENT);
-			contents.setBackground(Color.ORANGE);
+			contents.setOpaque(true);
+			contents.setBackground(MY_BG);
 
 			final JLabel author = new JLabel(user.getUsername());
 			Font font = author.getFont();
 			Font boldFont = new Font(font.getFontName(), Font.BOLD, font.getSize());
 			author.setFont(boldFont);
+			author.setForeground(INFO_TXT);
 
 			final Calendar cal = Calendar.getInstance();
 			final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 			final JLabel time = new JLabel(sdf.format(cal.getTime()));
+			time.setForeground(INFO_TXT);
 
-			// Do this before setting size..
-			messageContents.add(author, BorderLayout.WEST);
-			messageContents.add(contents, BorderLayout.CENTER);
-			messageContents.add(time, BorderLayout.EAST);
+			if (!user.equals(lastMessage)) {
+				messageContents.add(author, "wrap 1");
+			}
+			messageContents.add(contents, "pushx, growx"); // GROW FUCKER
+			messageContents.add(time);
 
-			// int x = chatLog.getWidth();
-			// int y = messageContents.getPreferredSize().height;
-			// Dimension d = new Dimension(x, y);
-			// messageContents.setPreferredSize(d);
-			// messageContents.setMaximumSize(d);
-			// messageContents.setMinimumSize(d);
-			// messageContents.setBackground(Color.BLUE);
-//			messageContents.setAlignmentX(Component.LEFT_ALIGNMENT);
+			chatLog.add(messageContents, "pushx, growx");
 
-			
-			GridBagConstraints c = new GridBagConstraints();
-			c.fill = GridBagConstraints.HORIZONTAL;
-			c.gridx = 0;
-			System.out.println(chatNum);
-			c.gridy = chatNum++;
-			c.anchor = GridBagConstraints.NORTHWEST;
-			c.weightx = 1;
-			c.weighty = 0;
-			
-			chatLog.add(messageContents, c);
 			chatLog.revalidate();
 			chatLog.repaint();
+
+			// auto scroll
+			int height = (int) chatLog.getPreferredSize().getHeight();
+			Rectangle rect = new Rectangle(0, height, 10, 10);
+			scrollChatLog.scrollRectToVisible(rect);
+			
+			lastMessage = user;
 		}
 	}
 }
